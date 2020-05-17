d9b2a86f24bb3b65b89ead0f5b2ce7fda7cefa7e
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2020-05-17 22:26:13 +0200

    Replaced Resolution option with quality

--- Assets/Scripts/OptionsManager.cs
+++ Assets/Scripts/OptionsManager.cs
@@ -7,41 +7,23 @@ using UnityEngine.UI;
 public class OptionsManager : MonoBehaviour
 {
 
-    public Dropdown ResolutionDropdown;
-
-    private List<Resolution> resolutions;
+    public Dropdown QualityDropdown;
 
     void Start()
     {
-        #region Resolution
-        this.resolutions = Screen.resolutions.ToList();
-
-        // Reverse resolution options to get best on top
-        this.resolutions.Reverse();
-
-        // Convert list of resolutions to list of strings
-        List<string> resolutionOptions = new List<string>();
-        foreach (Resolution resolution in this.resolutions)
-        {
-            resolutionOptions.Add($"{resolution.width} x {resolution.height}");
-        }
-
-        // Set initial value
-        ResolutionDropdown.ClearOptions();
-        ResolutionDropdown.AddOptions(resolutionOptions);
-        ResolutionDropdown.value = this.resolutions.FindIndex(x => (x.width == Screen.currentResolution.width && x.height == Screen.currentResolution.height));
-        ResolutionDropdown.RefreshShownValue();
-        #endregion Resolution
-    }
-
-    public void SetResolution(int index)
-    {
-        Resolution resolution = this.resolutions[index];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        QualityDropdown.ClearOptions();
+        QualityDropdown.AddOptions(QualitySettings.names.ToList());
+        QualityDropdown.value = QualitySettings.GetQualityLevel();
+        QualityDropdown.RefreshShownValue();
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetQualityLevel(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
     }
 }
