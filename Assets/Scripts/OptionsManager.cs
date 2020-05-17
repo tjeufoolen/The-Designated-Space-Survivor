2869587199bbcf7783a46d3c650da2d5696eb10a
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2020-05-17 22:35:02 +0200

    Set default value for fullscreen

--- Assets/Scripts/OptionsManager.cs
+++ Assets/Scripts/OptionsManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -8,13 +9,20 @@ public class OptionsManager : MonoBehaviour
 {
 
     public Dropdown QualityDropdown;
+    public Toggle FullscreenToggle;
 
     void Start()
     {
+        #region Quality
         QualityDropdown.ClearOptions();
         QualityDropdown.AddOptions(QualitySettings.names.ToList());
         QualityDropdown.value = QualitySettings.GetQualityLevel();
         QualityDropdown.RefreshShownValue();
+        #endregion Quality
+
+        #region Fullscreen
+        FullscreenToggle.isOn = Screen.fullScreen;
+        #endregion Fullscreen
     }
 
     public void SetFullscreen(bool isFullscreen)
