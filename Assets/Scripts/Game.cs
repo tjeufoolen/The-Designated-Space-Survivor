2a0d92731c948ee7bce460fcbf860d47093c8b94
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2020-05-22 16:53:49 +0200

    Added Distance traveled

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
     public GameObject GameOver;
+    public Camera Camera;
+    public Text TraveledDistanceText;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +19,19 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        updateDistanceTraveled();
+
         //TEMP
         if (Input.GetButtonDown("Jump"))
         {
             GameOver.SetActive(!GameOver.activeSelf);
         }
         //END TEMP
+    }
+
+    private void updateDistanceTraveled()
+    {
+        int distance = (int) Mathf.Ceil(Mathf.Abs(Camera.transform.position.x));
+        TraveledDistanceText.text = distance.ToString();
     }
 }
